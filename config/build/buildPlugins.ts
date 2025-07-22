78f398aaafcc00bf28b2790cf19ade24c4c7ae8f
marina-sheigets <sejmar1705@gmail.com>
2025-07-22 17:26:52 +0300

    feat: add mini-css-extract-plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,4 +1,5 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 
@@ -7,6 +8,10 @@ export function buildPlugins({ paths }: BuildOptions): webpack.WebpackPluginInst
         new webpack.ProgressPlugin(),
         new HtmlWebpackPlugin({
             template: paths.html
+        }),
+        new MiniCssExtractPlugin({
+            filename: "css/[name].[contenthash:8].css",
+            chunkFilename: "css/[name].[contenthash:8].css", // for async chunks (lazy loading)
         }),
     ]
 }
