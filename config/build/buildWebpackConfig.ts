60f38c72ff32d09d27ffec007d5e8ed42625de72
marina-sheigets <sejmar1705@gmail.com>
2025-07-22 16:58:58 +0300

    fix: toggle webpack dev fields

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -6,7 +6,7 @@ import { BuildOptions } from "./types/config";
 import webpack from "webpack";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const { mode, paths } = options;
+    const { mode, paths, isDev } = options;
     return {
         mode: mode,
         entry: paths.entry,
@@ -20,7 +20,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
             rules: buildLoaders()
         },
         resolve: buildResolvers(),
-        devtool: "inline-source-map",
-        devServer: buildDevServer(options),
+        devtool: isDev ? "inline-source-map" : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
     }
 }
