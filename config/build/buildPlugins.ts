ab06c5d95d6b4ae4bf7285c1d1793754c4999eda
marina-sheigets <sejmar1705@gmail.com>
2025-10-04 15:48:54 +0200

    fix: prod build in watch mode

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,25 +1,48 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import webpack from "webpack";
-import { BuildOptions } from "./types/config";
-import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+import HtmlWebpackPlugin from "html-webpack-plugin"
+import MiniCssExtractPlugin from "mini-css-extract-plugin"
+import webpack from "webpack"
+import { BuildOptions } from "./types/config"
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer"
 
-export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-    return [
+export function buildPlugins({
+    paths,
+    isDev
+}: BuildOptions): webpack.WebpackPluginInstance[] {
+    const basePlugins: webpack.WebpackPluginInstance[] = [
         new webpack.ProgressPlugin(),
+
         new HtmlWebpackPlugin({
             template: paths.html
         }),
+
         new MiniCssExtractPlugin({
             filename: "css/[name].[contenthash:8].css",
-            chunkFilename: "css/[name].[contenthash:8].css", // for async chunks (lazy loading)
+            chunkFilename: "css/[name].[contenthash:8].css"
         }),
+
         new webpack.DefinePlugin({
-            __IS_DEV__: JSON.stringify(isDev),
-        }),
+            __IS_DEV__: JSON.stringify(isDev)
+        })
+    ]
+
+    const devPlugins: webpack.WebpackPluginInstance[] = [
         new webpack.HotModuleReplacementPlugin(),
         new BundleAnalyzerPlugin({
             openAnalyzer: false,
-        }),
+            analyzerMode: "server"
+        })
+    ]
+
+    const prodPlugins: webpack.WebpackPluginInstance[] = [
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+            analyzerMode: "static",
+            reportFilename: "bundle-report.html"
+        })
+    ]
+
+    return [
+        ...basePlugins,
+        ...(isDev ? devPlugins : prodPlugins)
     ]
 }
