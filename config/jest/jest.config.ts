b8f6fc864765ad1fe157c77786ad1b7a17d01473
marina-sheigets <sejmar1705@gmail.com>
2025-10-05 17:15:00 +0200

    test: fix sidebar tests

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -3,73 +3,73 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type { Config } from 'jest';
+import type { Config } from "jest"
 
 const config: Config = {
-  // Automatically clear mock calls, instances, contexts and results before every test
-  clearMocks: true,
+    // Automatically clear mock calls, instances, contexts and results before every test
+    clearMocks: true,
 
-  // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: [
-    "\\\\node_modules\\\\"
-  ],
+    // An array of regexp pattern strings used to skip coverage collection
+    coveragePathIgnorePatterns: ["\\\\node_modules\\\\"],
+    // An array of directory names to be searched recursively up from the requiring module's location
+    moduleDirectories: ["node_modules"],
 
-  // An array of directory names to be searched recursively up from the requiring module's location
-  moduleDirectories: [
-    "node_modules"
-  ],
-
-  // An array of file extensions your modules use
-  moduleFileExtensions: [
-    "js",
-    "mjs",
-    "cjs",
-    "jsx",
-    "ts",
-    "mts",
-    "cts",
-    "tsx",
-    "json",
-    "node"
-  ],
-
-  // The root directory that Jest should scan for tests and modules within
-  rootDir: '../../',
-
-  // The test environment that will be used for testing
-  testEnvironment: "jsdom",
-
-  globals: {
-    __IS_DEV__: true,
-  },
-
-  // The glob patterns Jest uses to detect test files
-  testMatch: [
-    "<rootDir>/src/**/**/?(*.)+(spec|test).?([mc])[jt]s?(x)",
-    "<rootDir>/src/**/*.test.ts",
-    "<rootDir>/src/**/**/*.test.tsx"
-  ],
-
-  preset: "ts-jest/presets/default-esm",
-  extensionsToTreatAsEsm: [".ts", ".tsx"],
-
-  transform: {
-    "^.+\\.tsx?$": [
-      "ts-jest",
-      {
-        useESM: true,
-      },
+    // An array of file extensions your modules use
+    moduleFileExtensions: [
+        "js",
+        "mjs",
+        "cjs",
+        "jsx",
+        "ts",
+        "mts",
+        "cts",
+        "tsx",
+        "json",
+        "node"
     ],
-    "^.+\\.svg$": "<rootDir>/config/jest/svgTransform.js"
-  },
 
-  setupFilesAfterEnv: ["<rootDir>/config/jest/setupTests.ts"],
-  // recognize @/ as src/
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-    "\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
-    "\\.(css|sass|scss)$": "<rootDir>/config/jest/styleMock.ts"
-  },
-};
+    // The root directory that Jest should scan for tests and modules within
+    rootDir: "../../",
 
-export default config;
+    // The test environment that will be used for testing
+    testEnvironment: "jsdom",
+
+    globals: {
+        __IS_DEV__: true
+    },
+
+    // The glob patterns Jest uses to detect test files
+    testMatch: [
+        "<rootDir>/src/**/**/?(*.)+(spec|test).?([mc])[jt]s?(x)",
+        "<rootDir>/src/**/*.test.ts",
+        "<rootDir>/src/**/**/*.test.tsx"
+    ],
+
+    preset: "ts-jest/presets/default-esm",
+    extensionsToTreatAsEsm: [".ts", ".tsx"],
+
+    transform: {
+        "^.+\\.tsx?$": [
+            "ts-jest",
+            {
+                useESM: true
+            }
+        ],
+        "^.+\\.svg$":
+            "<rootDir>/config/jest/svgTransform.js"
+    },
+
+    setupFilesAfterEnv: [
+        "<rootDir>/config/jest/setupTests.ts"
+    ],
+    // recognize @/ as src/
+    moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/src/$1",
+        "\\.module\\.(css|sass|scss)$":
+            "identity-obj-proxy",
+        "\\.(css|sass|scss)$":
+            "<rootDir>/config/jest/styleMock.ts"
+    }
+}
+
+export default config
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,6 +1,6 @@
 import { userEvent } from "@testing-library/user-event";
 import { Sidebar } from "./Sidebar";
-import { renderWithTranslation } from "@/shared/lib/tests/renderWithTranslation/renderWithTranslation";
+import { renderComponent } from "@/shared/lib/tests/renderComponent/renderComponent";
 
 jest.mock("./Sidebar.module.scss", () => ({
   collapsed: "collapsed",
@@ -8,12 +8,12 @@ jest.mock("./Sidebar.module.scss", () => ({
 
 describe("Sidebar", () => {
   test("render", () => {
-    const screen = renderWithTranslation(<Sidebar />);
+    const screen = renderComponent(<Sidebar />);
     expect(screen.getByTestId("sidebar")).toBeInTheDocument();
   });
 
   test("toggle sidebar collapse", async () => {
-    const screen = renderWithTranslation(<Sidebar />);
+    const screen = renderComponent(<Sidebar />);
     const toggleButton = screen.getAllByRole("button")[0];
     await userEvent.click(toggleButton);
     expect(screen.getByTestId("sidebar")).toHaveClass(/collapsed/i);
