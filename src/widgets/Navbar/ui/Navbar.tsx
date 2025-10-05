137e94fcda09c8effb7a8c1943ce35b83eee87d8
marina-sheigets <sejmar1705@gmail.com>
2025-10-05 16:18:07 +0200

    chore: move links to sidebar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,28 +1,15 @@
 import { classNames } from "@/shared/lib/classNames/classNames";
 import * as styles from "./Navbar.module.scss";
-import { AppLink, AppLinkTheme } from "@/shared/ui/AppLink/AppLink";
-import { useTranslation } from "react-i18next";
 
 export interface NavbarProps {
   className?: string;
 }
 
 function Navbar({ className }: Readonly<NavbarProps>) {
-  const { t } = useTranslation();
 
   return (
     <div className={classNames(styles.navbar, {}, [className])}>
       <div className={styles.links}>
-        <AppLink
-          to="/"
-          className={styles.mainLink}
-          theme={AppLinkTheme.SECONDARY}
-        >
-          {t("Main")}
-        </AppLink>
-        <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>
-          {t("About")}
-        </AppLink>
       </div>
     </div>
   );
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,12 +4,16 @@ import { FC, useState } from "react";
 import { ThemeSwitcher } from "@/widgets/ThemeSwitcher";
 import { Button, ButtonSize, ButtonTheme } from "@/shared/ui/Button/Button";
 import { LanguageSwitcher } from "@/widgets/LanguageSwitcher";
+import { AppLink, AppLinkTheme } from "@/shared/ui/AppLink/AppLink";
+import { useTranslation } from "react-i18next";
+import { RoutePath } from "@/shared/config/routeConfig/routeConfig";
 
 export interface SidebarProps {
   className?: string;
 }
 
 export const Sidebar: FC<SidebarProps> = ({ className }) => {
+  const { t } = useTranslation();
   const [isCollapsed, setIsCollapsed] = useState(false);
 
   const onToggle = () => {
@@ -25,10 +29,30 @@ export const Sidebar: FC<SidebarProps> = ({ className }) => {
         [className]
       )}
     >
-      <Button size={ButtonSize.L} onClick={onToggle} className={styles.collapseBtn} theme={ButtonTheme.TRANSPARENT_INVERTED} square>
+      <Button
+        size={ButtonSize.L}
+        onClick={onToggle}
+        className={styles.collapseBtn}
+        theme={ButtonTheme.TRANSPARENT_INVERTED}
+        square
+      >
         {isCollapsed ? '>' : '<'}
       </Button>
-
+      <div className={styles.items}>
+        <AppLink
+          to={RoutePath.main}
+          className={styles.link}
+          theme={AppLinkTheme.SECONDARY}
+        >
+          {t("Main")}
+        </AppLink>
+        <AppLink
+          to={RoutePath.about}
+          className={styles.link}
+          theme={AppLinkTheme.SECONDARY}>
+          {t("About")}
+        </AppLink>
+      </div>
       <div className={styles.switchers}>
         <ThemeSwitcher />
         <LanguageSwitcher className={styles.languageSwitcher} isShort={isCollapsed} />
