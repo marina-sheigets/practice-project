4d2d1d141aab00038b49656cd6edb0d54f622fb9
marina-sheigets <sejmar1705@gmail.com>
2025-10-06 22:55:18 +0200

    feat: add login button and modal

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,16 +1,33 @@
 import { classNames } from "@/shared/lib/classNames/classNames";
 import * as styles from "./Navbar.module.scss";
+import { useTranslation } from "react-i18next";
+import { Button, ButtonTheme } from "@/shared/ui/Button/Button";
+import { useCallback, useState } from "react";
+import { Modal } from "@/shared/ui/Modal/Modal";
 
 export interface NavbarProps {
   className?: string;
 }
 
 function Navbar({ className }: Readonly<NavbarProps>) {
+  const { t } = useTranslation();
+  const [isAuthModalOpened, setIsAuthModalOpened] = useState(false);
+
+  const toggleAuthModal = useCallback(() => {
+    setIsAuthModalOpened((prev => !prev));
+  }, []);
 
   return (
     <div className={classNames(styles.navbar, {}, [className])}>
-      <div className={styles.links}>
-      </div>
+      <Button theme={ButtonTheme.CLEAR} className={styles.links} onClick={toggleAuthModal}>
+        {t("Login")}
+      </Button>
+      <Modal isOpened={isAuthModalOpened} onClose={toggleAuthModal}>
+        {
+          //  eslint-disable-next-line i18next/no-literal-string
+        }
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quis.
+      </Modal>
     </div>
   );
 }
