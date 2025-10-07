b3952f498ccdcfcc28f43b90e3ce1e03aa0c05ea
marina-sheigets <sejmar1705@gmail.com>
2025-10-07 23:16:32 +0200

    feat: add store provider for tests

--- src/shared/lib/tests/renderComponent/renderComponent.tsx
+++ src/shared/lib/tests/renderComponent/renderComponent.tsx
@@ -2,17 +2,21 @@ import { render } from "@testing-library/react";
 import { I18nextProvider } from "react-i18next";
 import { MemoryRouter } from "react-router-dom";
 import i18nForTests from "@/shared/config/i18n/i18nForTests";
+import { StoreProvider } from "@/app/providers/StoreProvider";
+import { StateSchema } from "@/app/providers/StoreProvider/config/StateSchema";
 
-
-export interface renderComponentOptions {
-    route?: string
+export interface RenderComponentOptions {
+    route?: string,
+    initialState?: Partial<StateSchema>
 }
 
-export function renderComponent(component: React.ReactNode, { route = '/' }: renderComponentOptions = {}) {
+export function renderComponent(component: React.ReactNode, { route = '/', initialState }: RenderComponentOptions = {}) {
 
     return render(
-        <MemoryRouter initialEntries={[route]}>
-            <I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
-        </MemoryRouter>
+        <StoreProvider initialState={initialState}>
+            <MemoryRouter initialEntries={[route]}>
+                <I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
+            </MemoryRouter>
+        </StoreProvider>
     );
 }
