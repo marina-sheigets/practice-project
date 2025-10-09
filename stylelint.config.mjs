3424b2d9c96464481189ad698bb0eafbea22d63c
marina-sheigets <sejmar1705@gmail.com>
2025-10-09 20:11:30 +0200

    fix: stylelint

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,10 +1,11 @@
-import stylelintConfigStandardScss from 'stylelint-config-standard-scss';
+import stylelintConfigStandardScss from "stylelint-config-standard-scss"
 
 export default {
     extends: [stylelintConfigStandardScss],
     rules: {
-        'at-rule-no-unknown': null,
-        'scss/at-rule-no-unknown': true,
-        'selector-class-pattern': null,
-    },
+        "at-rule-no-unknown": null,
+        "scss/at-rule-no-unknown": true,
+        "selector-class-pattern": null,
+        "rule-empty-line-before": null
+    }
 }
